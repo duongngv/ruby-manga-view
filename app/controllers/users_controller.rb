ab59b1a425ea3d360a767b7adeebb15504f64455
Duong Nguyen <nv.duongnv@gmail.com>
2019-07-09 22:00:23 +0700

    Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,38 @@
 class UsersController < ApplicationController
-  before_action :load_user, only: :show
+  before_action :load_user, :check_user, only: :show
 
   def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t ".success"
+      redirect_to login_path
+    else
+      flash[:danger] = t ".fails"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+  end
+
+  def check_user
+    return if load_user
+
+    flash[:danger] = t "common.not_found"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     get "login", to: "sessions#new"
     post "login", to: "sessions#create"
     delete "logout", to: "sessions#destroy"
+    get "signup", to: "users#new"
+    post "signup", to: "users#create"
 
     resources :users, except: %i(index destroy)
 
