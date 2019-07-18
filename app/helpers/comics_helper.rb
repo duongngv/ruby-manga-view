859aa2ecf314cb1cba74899c8253bff486e715e5
Duong Nguyen <nv.duongnv@gmail.com>
2019-07-18 17:06:06 +0700

    Fix layout display

--- app/helpers/comics_helper.rb
+++ app/helpers/comics_helper.rb
@@ -1,6 +1,6 @@
 module ComicsHelper
   def get_category category_id
-    return if (@category = Category.find_by id: category_id.to_s)
+    return unless (@category = Category.find_by id: category_id.to_s)
 
     @category.name
   end
