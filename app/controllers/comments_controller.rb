7a813f7cf74e52cb3eacaa7394e5c216ba8da154
nky4593 <nky451993@gmail.com>
2019-07-26 10:24:25 +0700

    Delete Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
-  before_action :load_chapter, :require_login, only: :create
+  before_action :load_chapter, only: :create
+  before_action :require_login, only: %i(create destroy)
+  before_action :correct_comment, only: :destroy
 
   def create
     @comment = @chapter.comments.build comment_params
@@ -11,6 +13,18 @@ class CommentsController < ApplicationController
         format.js
       end
     else
+      render "chapters/show"
+    end
+  end
+
+  def destroy
+    if @comment.destroy
+      respond_to do |format|
+        format.html{redirect_to request.referrer || @chapter}
+        format.js
+      end
+    else
+      flash[:danger] = t ".delete_failed"
       render "chapters/show"
     end
   end
@@ -27,5 +41,13 @@ class CommentsController < ApplicationController
     return if @chapter
     flash[:success] = t "common.chapter_not_found"
     redirect_to root_path
+  end
+
+  def correct_comment
+    @chapter = Chapter.find_by id: params[:chapter_id]
+    @comment = current_user.comments.find_by id: params[:id]
+    return if @comment
+    flash[:danger] = t ".not_found"
+    redirect_to @chapter
   end
 end
