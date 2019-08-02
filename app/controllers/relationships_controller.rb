9685def14ae53b1b7d550c21f7592a14905cfe36
Duong Nguyen <nv.duongnv@gmail.com>
2019-08-02 17:48:12 +0700

    Like Comic

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,27 +1,63 @@
 class RelationshipsController < ApplicationController
   before_action :require_login
+  before_action :load_comic, only: :create
+  before_action :load_following_comic, only: :destroy
 
   def create
-    return unless @comic = Comic.find_by(id: params[:comic_id])
-
-    if params[:follow] == Settings.follow
+    if follow?
       current_user.follow @comic
       respond_to do |format|
         format.html{redirect_to @comic}
-        format.js
+        format.js{render "follow.js.erb"}
+      end
+    elsif like?
+      current_user.like @comic
+      respond_to do |format|
+        format.html{redirect_to @comic}
+        format.js{render "like.js.erb"}
       end
     end
   end
 
   def destroy
-    return unless @comic = Relationship.find_by(id: params[:id]).comic
-
-    if params[:follow] == Settings.follow
+    if follow?
       current_user.unfollow @comic
       respond_to do |format|
         format.html{redirect_to @comic}
-        format.js
+        format.js{render "unfollow.js.erb"}
+      end
+    elsif like?
+      current_user.unlike @comic
+      respond_to do |format|
+        format.html{redirect_to @comic}
+        format.js{render "unlike.js.erb"}
       end
     end
+  end
+
+  private
+
+  def follow?
+    params[:follow].eql? Settings.follow
+  end
+
+  def like?
+    params[:like].eql? Settings.like
+  end
+
+  def load_following_comic
+    @comic = Relationship.find_by(id: params[:id]).comic
+
+    return if @comic
+    flash[:warning] = t ".not_found"
+    redirect_to root_path
+  end
+
+  def load_comic
+    @comic = Comic.find_by(id: params[:comic_id])
+
+    return if @comic
+    flash[:warning] = t ".not_found"
+    redirect_to root_path
   end
 end
